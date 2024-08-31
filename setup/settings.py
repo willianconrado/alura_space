1a406187501e58650c230357d39ee3612d429685
Willian Conrado <willian.conrado@gmail.com>
2024-08-31 19:12:20 -0300

    Ajustando o gitignore

--- setup/settings.py
+++ setup/settings.py
@@ -10,8 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-import os
-from pathlib import Path
+from pathlib import Path, os
 from dotenv import load_dotenv
 
 load_dotenv()
